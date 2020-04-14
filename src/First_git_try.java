eb27a105813830021c0c4bc584114cf3244acb9b
JoaoFonseca99 <62515654+JoaoFonseca99@users.noreply.github.com>
2020-04-14 20:15:46 +0100

    Update First_git_try.java

--- src/First_git_try.java
+++ src/First_git_try.java
@@ -4,6 +4,7 @@ public class First_git_try {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Pro 20!!");
+System.out.println("Velhote <3");
 	}
 
 }
